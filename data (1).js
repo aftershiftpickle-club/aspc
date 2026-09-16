0300c2dbfc2c0b45d3d2ba664107b1682f2475c8
aftershiftpickle-club <aftershift.club@gmail.com>
2026-09-16 10:36:54 +0400

    Update data (1).js

--- data (1).js
+++ data (1).js
@@ -270,6 +270,20 @@ const ALBUMS = [
     url: "https://drive.google.com/drive/folders/1ICyb5uV0frpyunOKOsTdq_O1xZaHVeSH?usp=drive_link",
     cover: ""
   }, 
+   {
+    date: "2026-09-11",
+    title: "Sept 11, 2026 Open Play",
+    description: "Regular and Ascend.",
+    url: "https://drive.google.com/drive/folders/1zwYg3nRLVPJIOV2npW5IgtT24Efzdusn?usp=drive_link",
+    cover: ""
+  }, 
+   {
+    date: "2026-09-15",
+    title: "Sept 15, 2026 Social Play",
+    description: "Regular and Ascend.",
+    url: "https://drive.google.com/drive/folders/1ZYI2ntfxvEaQ_X85xyoc8ALWB4LyGmjT?usp=drive_link",
+    cover: ""
+  }, 
    
 ];
 
